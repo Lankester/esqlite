755222de319e30bfcb3d1c5541dd0879f46ab269
Paul Carew <paul@carewrx.com>
2020-12-03 17:25:46 +0000

    Bump to SQLite 3.34.0

--- test/esqlite_test.erl
+++ test/esqlite_test.erl
@@ -360,14 +360,14 @@ sqlite_version_test() ->
     {ok, Db} = esqlite3:open(":memory:"),
     {ok, Stmt} = esqlite3:prepare("select sqlite_version() as sqlite_version;", Db),
     {sqlite_version} =  esqlite3:column_names(Stmt),
-    ?assertEqual({row, {<<"3.27.2">>}}, esqlite3:step(Stmt)),
+    ?assertEqual({row, {<<"3.34.0">>}}, esqlite3:step(Stmt)),
     ok.
 
 sqlite_source_id_test() ->
     {ok, Db} = esqlite3:open(":memory:"),
     {ok, Stmt} = esqlite3:prepare("select sqlite_source_id() as sqlite_source_id;", Db),
     {sqlite_source_id} =  esqlite3:column_names(Stmt),
-    ?assertEqual({row, {<<"2019-02-25 16:06:06 bd49a8271d650fa89e446b42e513b595a717b9212c91dd384aab871fc1d0f6d7">>}}, esqlite3:step(Stmt)),
+    ?assertEqual({row, {<<"2020-12-01 16:14:00 a26b6597e3ae272231b96f9982c3bcc17ddec2f2b6eb4df06a224b91089fed5b">>}}, esqlite3:step(Stmt)),
     ok.
 
 garbage_collect_test() ->
